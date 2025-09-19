3f525c74971d1ea427547a47ff3c8a1358e89c5d
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 12:42:10 +0530

    49194

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ BASE_URL = "http://127.0.0.1:8000"
 
 def fetch_menu_categories():
     try:
-        response = requests.get(f"{BASE_URL}/menu-categories/")
-        response.raise_for_status()  # Raises HTTPError for bad responses
+        response = requests.get(f"{BASE_URL}/menu-categories/",timeout=5)
+        response.raise_for_status()
+        
         categories = response.json()
         print("Menu Categories:")
         for category in categories:
             print(f"- {category['name']}")
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.HttpError as http_err:
+        print(f"HTTP error occured:{http_err}")
     except requests.exceptions.ConnectionError:
         print("Error: Could not connect to the server.")
-    except requests.exceptions.Timeout:
-        print("Error: Request timed out.")
-    except requests.exceptions.RequestException as err:
+    except request.exceptions.Timeout:
+        print("Error: Request time out.")
+    except request.exceptions.RequestException as err:
         print(f"An error occurred: {err}")
-
+        
 if __name__ == "__main__":
     fetch_menu_categories()
