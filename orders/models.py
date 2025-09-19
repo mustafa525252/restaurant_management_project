aebb73b4a7f5b62194dc5dc53d17c3ee0d21f6d6
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 19:54:02 +0530

    49191

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from .models import OrderStatus
 from django.config.auth.models import User
 
 # Create your models here.
+
+class OrderStatus(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    
+    class Meta:
+        verbose_name="Order Status"
+        verbose_name_plural="Order Statuses"
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     customer=models.ForeignKey(
@@ -13,7 +22,7 @@ class Order(models.Model):
     order_date=models.DateTimeField(auto_now_add=True)
 
     status=model.ForeignKey(
-        OrderStatus,
+        "OrderStatus",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
