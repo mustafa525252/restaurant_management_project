37a4523144a44deca57dae2f223909937d41576d
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 18:37:25 +0530

    49194

--- home/models.py
+++ home/models.py
@@ -1,21 +1,15 @@
-"""
-models.py
-This module defines the database models for the application.
-"""
 from django.db import models
 
-# Create your models here.
-
 class MenuCategory(models.Model):
-    name=models.CharField(max_length=100,unique=True)
+    name=models.CharField(
+        max_length=100,
+        unique=True
+    )
+
     class Meta:
         verbose_name = "Menu Category"
         verbose_name_plural = "Menu Categories"
         ordering = ["name"]
-        
+
     def __str__(self):
-        """
-        Return a human-readable string representation of the category.
-        This will display as the category name in admin and Django Shell.
-        """
         return self.name
