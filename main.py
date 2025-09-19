0a45bb376fad9c5214f9b186942e048736c9f921
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 11:10:31 +0530

    49194

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+
+def fetch_menu_categories():
+    # Fetch all menu categories from the API endpoint.
+    response = request.get(f"{BASE_URL}/menu-categories/")
+    if response.status_code = 200:
+        print("Menu Categories:")
+        for category in response.json():
+            print(f"- {category['name']}")
+    else:
+        print(f"Failed to fetch categories. Status code:{response.status_code}")
+if __name__ == "__main__":
+    fetch_menu_categories()
