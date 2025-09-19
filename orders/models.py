bfb08bf050b6bd41bbf630eb51ec36567d292f7f
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 19:48:24 +0530

    49191

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from .models import OrderStatus
+import django.config.auth.models import User
 
 # Create your models here.
 
 class Order(models.Model):
-    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    customer=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='orders'
+        )
     order_date=models.DateTimeField(auto_now_add=True)
 
     status=model.ForeignKey(
@@ -17,3 +22,6 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.status.name if self.status else 'No Status'}"
+
+    class Meta:
+        ordering=['-order_date']
