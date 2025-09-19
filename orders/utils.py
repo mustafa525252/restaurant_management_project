9b0ff9a9eee3d3b58967a20c5785b6a4a2a727b1
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 13:15:02 +0530

    49193

--- orders/utils.py
+++ orders/utils.py
@@ -1,1 +1,15 @@
 
+
+
+import string
+import secrets
+from orders.models import Coupon #Make sure you have coupon model
+
+def generate_coupon_code(length=10):
+    characters = string.ascii_uppercase + string.digits
+
+    while True:
+        code = ''.join(secrets.choice(characters) for _ in range(length))
+
+        if not Coupon.objects.filter(code=code).exists():
+            return code
