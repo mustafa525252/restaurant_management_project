c98b65ea95909992cdecf040aea48f48213e6d80
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 20:03:38 +0530

    49192

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,5 @@
+
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -17,7 +19,7 @@ class Order(models.Model):
     customer = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
-        related_name = 'orders'
+        related_name = 'customer_orders'
         )
     order_date=models.DateTimeField(auto_now_add = True)
 
@@ -26,7 +28,7 @@ class Order(models.Model):
         on_delete=models.SET_NULL,
         null = True,
         blank = True,
-        related_name = 'orders'
+        related_name = 'status_orders'
     )
 
     def __str__(self):
