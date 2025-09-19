e63a9eade3b8adc627e555270c6f149cca7a7a26
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 13:42:07 +0530

    49193

--- orders/utils.py
+++ orders/utils.py
@@ -3,6 +3,6 @@ import string
 
 COUPON_CHARACTERS = string.ascii_uppercase + string.digits
 
-def generate_coupon_code(length=10):
-    code = ''.join(secrets.choice(COUPON_CHARACTERS) for _ in range(length))
-    return code
+def generate_unique_coupon_code(length=10):
+    coupon_code = ''.join(secrets.choice(COUPON_CHARACTERS) for _ in range(length))
+    return coupon_code
