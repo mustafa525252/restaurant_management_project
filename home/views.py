611fa35895c43f433f99b8dfdf892e08cded886f
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 10:38:26 +0530

    49194

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
 
 # Create your views here.
+
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
