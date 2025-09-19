845c413204874d35137546dccfae332bcdc72dcb
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 19:45:35 +0530

    49191

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .models import OrderStatus
 
 # Create your models here.
+
+class Order(models.Model):
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    order_date=models.DateTimeField(auto_now_add=True)
+
+    status=model.ForeignKey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.status.name if self.status else 'No Status'}"
