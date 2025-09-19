ca19b30b6b3cee620d7326586964f3ab21009ea4
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-19 14:01:58 +0530

    49192

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_default_order_statuses(sender,**kwargs):
+    if sender.name == 'orders':
+        for status in ['Pending','Processing','Completed','Cancelled']:
+            OrderStatus.objects.get_or_create(name=status)
