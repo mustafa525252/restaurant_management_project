535152c6b089d0660e8632e607a32681803c090e
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-20 11:19:03 +0530

    49192

--- orders/models.py
+++ orders/models.py
@@ -15,10 +15,10 @@ class OrderStatus(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(
+    customer     = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='customer_orders'
     )
-    order_date = models.DateTimeField(auto_now_add=True)
+    order_date   = models.DateTimeField(auto_now_add=True)
     order_status = models.ForeignKey(
         OrderStatus,
         on_delete=models.SET_NULL,
@@ -27,9 +27,9 @@ class Order(models.Model):
         related_name='status_orders'
     )
 
-    name = models.CharField(max_length=100)
+    name     = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price    = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return f"Order #{self.id} - {self.order_status.name if self.order_status else 'No Status'}"
