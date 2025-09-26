a977413e336209cb1782d13f7ab441f4c22b9ffb
mustafa525252 <118329799+mustafa525252@users.noreply.github.com>
2025-09-26 13:35:24 +0530

    50081

--- home/models.py
+++ home/models.py
@@ -19,3 +19,20 @@ class MenuCategory(models.Model):
     def __str__(self):
         # Return the category name as its string representation.
         return self.name
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(
+        MenuCategory,
+        on_delete = models.CASCADE,
+        related_name = "menu_items"
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
