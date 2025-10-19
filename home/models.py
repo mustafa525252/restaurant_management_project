b2dc61ae4e6c9dc6dcf8c5af7ce8b909f3e97988
Mustafa Attarwala <attarwala2004@gmail.com>
2025-10-19 13:46:44 +0530

    Add a Model Field for Restaurant Opening Days

--- home/models.py
+++ home/models.py
@@ -36,3 +36,16 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact_number = models.CharField(max_length=20)
+    # New field for storing operating days
+    operating_days = models.CharField(
+        max_length=100,
+        help_text="Comma-separated days, e.g. 'Mon,Tue,Wed,Thu,Fri'"
+    )
+
+    def __str__(self):
+        return self.name
