78cb0766f786f6f04d75a4ddf68581135da6e6f2
Mustafa Attarwala <attarwala2004@gmail.com>
2025-10-31 11:26:56 +0530

    Create a Customer Loyalty Program Model

--- orders/models.py
+++ orders/models.py
@@ -192,3 +192,14 @@ class PaymentMethod(models.Model):
 
     def __str__(self):
         return self.name
+    
+class LoyaltyProgram(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    points_per_dollar_spent = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.points_per_dollar_spent} pts/$"
