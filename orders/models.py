fa9a70dbefcfff589b527fe80d63617590b75361
Mustafa Attarwala <attarwala2004@gmail.com>
2025-12-01 12:41:19 +0530

    Create a Discount Model

--- orders/models.py
+++ orders/models.py
@@ -257,3 +257,15 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user_name} - {self.rating}⭐"
+
+
+
+class Discount(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.code} ({self.percentage}%)"
