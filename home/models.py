0880b251c35fcd8db1668544daa673558053daca
Mustafa Attarwala <attarwala2004@gmail.com>
2025-12-11 09:29:05 +0530

    Create an Ingredient Model

--- home/models.py
+++ home/models.py
@@ -460,3 +460,11 @@ class DeliveryZone(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    unit_of_measure = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
