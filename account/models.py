0b42ee787b2b5ff7cbffcc4fccfb02730c82760c
Mustafa Attarwala <attarwala2004@gmail.com>
2025-12-16 10:25:21 +0530

    Extend User Model for Customer Profile

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 CUISINE_CHOICES = (
     ('italian', 'Italian'),
@@ -32,3 +33,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="customer_profile"
+    )
+    phone_number = models.CharField(max_length=15)
+    delivery_address = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - Customer Profile"
